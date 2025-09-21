52a19d856dfc51ac9f68dff37c3290a52530b835
faithskies <faithskies@gmail.com>
2025-09-20 22:34:08 -0400

    Majorly updated the comments. Rearranged  each test to include const first.

--- LoopEvaluation/pages/login.js
+++ LoopEvaluation/pages/login.js
@@ -1,33 +1,27 @@
 exports.LoginPage = class LoginPage {
+     //---- This class is meant to store any values & methods in regards to the login page
 
-   constructor(page){
-    // important variables  - this could be moved to a separate page but, there arent many at this point
+     constructor(page) {
+          this.page = page
+          this.testWebsite = 'https://animated-gingersnap-8cf7f2.netlify.app/'
+          this.username_textbox = page.getByRole('textbox', { name: 'Username' })
+          this.password_textbox = page.getByRole('textbox', { name: 'Password' })
+          this.login_button = page.getByRole('button', { name: 'Sign in' })
+     }
 
-    this.page = page
-    this.username_textbox = page.getByRole('textbox', { name: 'Username' })
-    this.password_textbox = page.getByRole('textbox', { name: 'Password' })
-    this.login_button = page.getByRole('button', { name: 'Sign in' })
-   }
+     // use this method if you want to pass different [aka invalid] values for some negative tests.. or if you need to sign in as a different user 
+     async login(username, password) {
+          await this.page.goto(this.testWebsite);
+          await this.username_textbox.fill(username)
+          await this.password_textbox.fill(password)
+          await this.login_button.click()
+     }
 
-   async gotoLoginPage(){
-    await this.page.goto('https://animated-gingersnap-8cf7f2.netlify.app/');
-    // note this is not currently used.. but you could use this for a simple test case of login elemements 
-   }
-
-
-   async login(username, password){
-        await this.page.goto('https://animated-gingersnap-8cf7f2.netlify.app/');
-        await this.username_textbox.fill(username)
-        await this.password_textbox.fill(password)
-        await this.login_button.click()
-
-   }
-
-    async succesfulLogin(){
-        const valid_username = 'admin'
-        const valid_password = 'password123'
-        await this.login(valid_username, valid_password)
-
-   }
+     // use this method if the end goal is being logged in without having to pass any values
+     async succesfulLogin() {
+          const valid_username = 'admin'
+          const valid_password = 'password123'
+          await this.login(valid_username, valid_password)
+     }
 
 }
