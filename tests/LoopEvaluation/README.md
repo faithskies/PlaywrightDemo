c7b8a2643c8e6168d8f9de896687ae2f563fdfb8
faithskies <faithskies@gmail.com>
2025-09-23 23:53:12 -0400

    Added a folder for example API tests in Playwright

--- tests/LoopEvaluation/README.md
+++ tests/LoopEvaluation/README.md
@@ -1,8 +1,8 @@
 # PlaywrightDemo
 
-This folder is specifically for evaluation based on the 6 proposed tests. 
+This folder is specifically for evaluation based 6 requested tests.
 
-For reference, I have used JavaScript quite a bit for API testing but this is my first time ever using Playwright. Its been about two days since I started this project. I know there are probably better ways to go about this and Id love to get the opportunity to learn further and adopt your preferred formats. 
+For reference, I have used JavaScript quite a bit for API testing but this is my first time ever using Playwright. Its been about two days since I started this project. I know there are probably better ways to go about this and Id love to get the opportunity to learn further.
 
 I set up a GitHub project with a main and testbranch branch 
 When I made some significant progress I pushed my tests to the  testbranch and when it was stable I pulled those changes to my main branch.
--- tests/PlaywrightAPIDemo/test/api_tests.spec.js
+++ tests/PlaywrightAPIDemo/test/api_tests.spec.js
@@ -1,7 +1,90 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test ('API GET Request', async ({request}) => {
+// created this demo using tutorial from https://www.youtube.com/watch?v=EEjyLfp6DoQ&list=PLhW3qG5bs-L9sJKoT1LC5grGT77sfW0Z8&index=15
 
-    const response = await request.get('https://reqres.in/api/users/2')
+// of note I had to deviate from the tutorial by adding a header, since they were not previously required
 
+
+// A PUT request is an HTTP method used to create or replace a resource on a server with new data
+test('API DELETE Request', async ({ request }) => {
+
+    const response = await request.delete('https://reqres.in/api/users/2', {
+        headers: {
+            'x-api-key': 'reqres-free-v1',
+        },
+
+        data: {
+            "name": "Thomas",
+            "job": "QA Manager"
+        }
+    })
+    expect(response.status()).toBe(204)
 })
+
+
+
+// A PUT request is an HTTP method used to create or replace a resource on a server with new data
+test('API Put Request', async ({ request }) => {
+
+    const response = await request.put('https://reqres.in/api/users/2', {
+        headers: {
+            'x-api-key': 'reqres-free-v1',
+        },
+
+        data: {
+            "name": "Thomas",
+            "job": "QA Manager"
+        }
+    })
+    expect(response.status()).toBe(200)
+    const text = await response.text();
+    expect(text).toContain('QA Manager')
+    expect(text).toContain('Thomas')
+
+    console.log(await response.json());
+})
+
+// A POST request is an HTTP request method used to send data to a server to create or update a resource.
+// Data in the body: The data being sent is contained within the body of the HTTP request, not in the URL. This allows for sending larger amounts of data and more sensitive information, as it's not exposed in the URL.
+
+test('API Post Request', async ({ request }) => {
+
+    const response = await request.post('https://reqres.in/api/users', {
+        headers: {
+            'x-api-key': 'reqres-free-v1',
+        },
+
+        data: {
+            "name": "Thomas",
+            "job": "QA Lead"
+        }
+    })
+
+    expect(response.status()).toBe(201)
+    const text = await response.text();
+    expect(text).toContain('QA Lead')
+    expect(text).toContain('Thomas')
+
+    console.log(await response.json());
+})
+
+// GET request is an HTTP method used to request data from a specified resource on a web server. 
+// It is one of the most common HTTP methods and is primarily used for retrieving information.
+test('API GET Request', async ({ request }) => {
+    const response = await request.get('https://reqres.in/api/users/2', {
+        headers: {
+            'x-api-key': 'reqres-free-v1',
+        },
+
+        data: {
+            "name": "Thomas",
+            "job": "QA Lead"
+        }
+    })
+    expect(response.status()).toBe(200)
+    const text = await response.text();
+    expect(text).toContain('Janet')
+
+    console.log(await response.json());
+})
+
