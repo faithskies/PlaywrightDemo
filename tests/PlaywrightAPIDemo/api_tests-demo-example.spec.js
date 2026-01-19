95835b9f8d0724b5a5459032f1bddab8956d18ef
faithskies <faithskies@gmail.com>
2026-01-18 22:03:57 -0500

    Commenting out some of the API tests as the associated site has changed the API key requirements

--- tests/PlaywrightAPIDemo/api_tests-demo-example.spec.js
+++ tests/PlaywrightAPIDemo/api_tests-demo-example.spec.js
@@ -4,69 +4,70 @@ import { test, expect } from '@playwright/test'
 
 // of note I had to deviate from the tutorial by adding a header, since they were not previously required
 
-
+//COMMENTING THIS OUT. reques has changed the API key requirements.. 
 // A PUT request is an HTTP method used to create or replace a resource on a server with new data
-test('API DELETE Request', async ({ request }) => {
+// test('API DELETE Request', async ({ request }) => {
+//     const response = await request.delete('https://reqres.in/api/users/2', {
+//         headers: {
+//             'x-api-key': 'reqres-free-v1',
+//         },
 
-    const response = await request.delete('https://reqres.in/api/users/2', {
-        headers: {
-            'x-api-key': 'reqres-free-v1',
-        },
-
-        data: {
-            "name": "Thomas",
-            "job": "QA Manager"
-        }
-    })
-    expect(response.status()).toBe(204)
-})
-
+//         data: {
+//             "name": "Thomas",
+//             "job": "QA Manager"
+//         }
+//     })
+//     expect(response.status()).toBe(204)
+// })
 
 
+//COMMENTING THIS OUT. reques has changed the API key requirements.. 
 // A PUT request is an HTTP method used to create or replace a resource on a server with new data
-test('API Put Request', async ({ request }) => {
+// test('API Put Request', async ({ request }) => {
 
-    const response = await request.put('https://reqres.in/api/users/2', {
-        headers: {
-            'x-api-key': 'reqres-free-v1',
-        },
+//     const response = await request.put('https://reqres.in/api/users/2', {
+//         headers: {
+//             'x-api-key': 'reqres-free-v1',
+//         },
 
-        data: {
-            "name": "Thomas",
-            "job": "QA Manager"
-        }
-    })
-    expect(response.status()).toBe(200)
-    const text = await response.text();
-    expect(text).toContain('QA Manager')
-    expect(text).toContain('Thomas')
+//         data: {
+//             "name": "Thomas",
+//             "job": "QA Manager"
+//         }
+//     })
+//     expect(response.status()).toBe(200)
+//     const text = await response.text();
+//     expect(text).toContain('QA Manager')
+//     expect(text).toContain('Thomas')
 
-    console.log(await response.json());
-})
+//     console.log(await response.json());
+// })
 
+
+//COMMENTING THIS OUT. reques has changed the API key requirements.. 
 // A POST request is an HTTP request method used to send data to a server to create or update a resource.
 // Data in the body: The data being sent is contained within the body of the HTTP request, not in the URL. This allows for sending larger amounts of data and more sensitive information, as it's not exposed in the URL.
 
-test('API Post Request', async ({ request }) => {
+// test('API Post Request', async ({ request }) => {
 
-    const response = await request.post('https://reqres.in/api/users', {
-        headers: {
-            'x-api-key': 'reqres-free-v1',
-        },
+//     const response = await request.post('https://reqres.in/api/users', {
+//         headers: {
+//             'x-api-key': 'reqres-free-v1',
+//         },
 
-        data: {
-            "name": "Thomas",
-            "job": "QA Lead"
-        }
-    })
+//         data: {
+//             "name": "Thomas",
+//             "job": "QA Lead"
+//         }
+//     })
 
-    expect(response.status()).toBe(201)
-    const text = await response.text();
-    expect(text).toContain('QA Lead')
-    expect(text).toContain('Thomas')
+//     expect(response.status()).toBe(201)
+//     const text = await response.text();
+//     expect(text).toContain('QA Lead')
+//     expect(text).toContain('Thomas')
 
-    console.log(await response.json());
-})
+//     console.log(await response.json());
+// })
 
 // GET request is an HTTP method used to request data from a specified resource on a web server. 
 // It is one of the most common HTTP methods and is primarily used for retrieving information.
